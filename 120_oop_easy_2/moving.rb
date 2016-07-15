f4d485a02a81a0bb635d1c595a671fb473254e5b
bcasadei <bill@billcasadei.com>
2016-07-14 20:34:58 -0600

    Launch School Exercises

---
+++ 120_oop_easy_2/moving.rb
@@ -0,0 +1,51 @@
+class Mammal
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def walk
+    puts "#{name} #{gait} forward"
+  end
+end
+
+class Person < Mammal
+  
+  private
+
+  def gait
+    "strolls"
+  end
+end
+
+
+class Cat < Mammal
+
+  private
+
+  def gait
+    "saunters"
+  end
+end
+
+class Cheetah < Mammal
+
+  private
+
+  def gait
+    "runs"
+  end
+end
+
+mike = Person.new("Mike")
+mike.walk
+# => "Mike strolls forward"
+
+kitty = Cat.new("Kitty")
+kitty.walk
+# => "Kitty saunters forward"
+
+flash = Cheetah.new("Flash")
+flash.walk
+# => "Flash runs forward"
